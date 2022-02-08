f40125e2fc81a10745303ef1f0137a03c76743ad
harelf3 <alonlalon123@gmail.com>
2022-02-08 13:54:50 +0200

    second commit

--- productivity/models.py
+++ productivity/models.py
@@ -9,17 +9,17 @@ class Data(models.Model):
     status = models.BooleanField(default=False)
 
 class Connections(models.Model):
-    full_name = models.CharField(max_length=10)
-    contact = models.CharField(max_length=10)
-    title = models.CharField(max_length=30)
-    desc = models.CharField(max_length=30)
-    field = models.CharField(max_length=30)
+    full_name = models.CharField(max_length=30)
+    contact = models.CharField(max_length=70)
+    title = models.CharField(max_length=40)
+    desc = models.CharField(max_length=140)
+    field = models.CharField(max_length=40)
 
 
 class NetworkEvents(models.Model):
     event_type = models.CharField(max_length=10)
-    website_name = models.CharField(max_length=30)
-    website_url = models.CharField(max_length=100)
+    website_name = models.CharField(max_length=50)
+    website_url = models.CharField(max_length=140)
     importance = models.IntegerField(default=2)
 
 
